be5b9a4ca8314ac62444f1a100a16a98ff53cee9
Nathan Turen <nathanturen@yahoo.com>
2024-09-19 10:48:59 -0500

    Added eventSearch and profileForm, also make sure to npm install @mui/x-date-pickers@latest date-fns@2.x since I am using that for my date picking

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Login} from './components/login';
 import { Register } from './components/register';
+import { ProfileForm } from './components/profileForm';
+import { EventManagementForm } from './components/eventSearch';
 
 export const App = () => {
     return (
@@ -9,6 +11,8 @@ export const App = () => {
         <Routes>
             <Route path="/" Component={Login}/>
             <Route path="/register" Component={Register}/>
+            <Route path = "/profileForm" Component = {ProfileForm}/>
+            <Route path = "/event" Component = {EventManagementForm}/>
         </Routes>
         </BrowserRouter>
     );
